9c30d34589c6deb783049dab17280896ed4a03a5
Supreecha Nakdang <supreecha@ideractive.com>
2016-10-21 14:02:04 +0700

    [Chat] Index Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root :to => "chat#index"
+
+  controller :chat do
+    get 'chat/index'  => :index
+  end
+
+  # root 'chat/#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
