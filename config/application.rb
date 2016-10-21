675636950b0977a62f09ffc35169a9d6ab17bcd0
Supreecha Nakdang <supreecha@ideractive.com>
2016-10-21 16:23:49 +0700

    .ignore

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module RealtimeChat
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.web_console.whitelisted_ips = '192.168.1.240'
   end
 end
